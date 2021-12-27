0b68d28bba98e267e151c70d5a745b9d091a2070
Waldyr Turquetti <waldyr.tg@gmail.com>
2021-12-27 10:44:27 -0300

    feat: finalizado os videos do capitulo 5.

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -19,56 +19,37 @@ public class RotaPedidos {
 
 				from("file:pedidos?delay=5s&noop=true")
 						.routeId("rota-pedidos")
-						//Processamento síncrono
-//						.multicast()			//Envia o mesmo body pras duas rotas
-//						.parallelProcessing()	//Cada subrota é processada paralelamente
-//						.timeout(500)			//Define um timeout no processamento
-//					.to("direct:http") 			//Sub rota
-//					.to("direct:soap");			//Sub rota
-					//Processamento assíncrono
-					.to("seda:http")
-					.to("seda:soap");
+						.multicast()
+					.to("direct:http")
+					.to("direct:soap");
 
-				//Chamando o serviço http.
-				//Processamento síncrono
-//				from("direct:http")
-				//Processamento assíncrono
-				from("seda:http")
+				from("direct:http")
 					.routeId("rota-http")
 						.setProperty("pedidoId", xpath("/pedido/id/text()"))
 						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
 						.split().xpath("/pedido/itens/item")
-							.log("${id} - ${body}")
+//							.log("${id} - ${body}")
 						.filter().xpath("/item/formato[text()='EBOOK']")
 						.setProperty("ebookId", xpath("/item/livro/codigo/text()"))
-							.log("${id} - ${body}")
+//							.log("${id} - ${body}")
 						.marshal().xmljson()
-							.log("${id} - ${body}")
+//							.log("${id} - ${body}")
 						.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
 						.setHeader(Exchange.HTTP_QUERY,
 								simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
 					.to("http4://localhost:8080/webservices/ebook/item");
 
-				//Chamando o serviço soap.
-				//Processamento síncrono
-//				from("direct:soap")
-				//Processamento assíncrono
-				from("seda:soap")
+				from("direct:soap")
 					.routeId("rota-soap")
+						.to("xslt:pedido-para-soap.xslt")
 						.log("${body}")
-						.setBody(constant("<envelope>teste<envelope>"))
-						.log("${body}")
-					.to("mock:soap");//O mock simula um endpoint.
+						.setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
+					.to("http4://localhost:8080/webservices/financeiro");
 
 			}
 		});
 
 		context.start();
-
-		//Podemos enviar uma mensagem programaticamente para um rota que começa com direct.
-//		ProducerTemplate producer = context.createProducerTemplate();
-//		producer.sendBody("direct:soap", "<pedido> ... </pedido>");
-
 		Thread.sleep(2000);
 		context.stop();
 	}	
